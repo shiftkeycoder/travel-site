15de42c16e4c26d3a3e0de724be5e0ae8539e9a4
CalebH <chronos318@yahoo.com>
2020-08-26 16:43:12 -0400

    @MobileMenu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,13 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import Engine from "./include/Engine";
+import MobileMenu from './include/MobileMenu';
 
 const app = new Engine;
 
 app.hotInject();
 app.vueInit();
+
+new MobileMenu();
 
 
